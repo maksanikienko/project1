a68564ba3702bd185608ae58f3256196a941e2bc
Alexandr Harabara <dev.xandr@gmail.com>
2022-07-25 17:40:21 +0300

    Task 1 - first function usage

--- task_1.php
+++ task_1.php
@@ -1,19 +1,29 @@
 <?php
-$row = 1;
 //[ "task_1.php", "user.csv", "Andrew" ] <= php task_1.php user.csv Andrew
   [ $script,      $filename,  $username] = $argv;
 
 print "Searching $username in $filename".PHP_EOL;
 
-if (($handle = fopen($filename, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-        if (str_contains($data[0], $username)) {
-            echo "$row : " . json_encode($data) . PHP_EOL . "\n";
+// (filename, name) => user[]
+function filter_by_name(string $filename, string $username): array
+{
+    $result = []; // [stringa, stringb, 1,2,3,]
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+            if (str_contains($data[0], $username)) {
+                $result[] = $data;
+                // array_push($result, $data);
+            }
         }
-        $row++;
+        fclose($handle);
     }
-    fclose($handle);
+
+    return $result;
 }
+
+$results = filter_by_name($filename, $username);
+
+print_r(array_map('json_encode', $results));
 
 // A => A'
 // [..] | [..]'
