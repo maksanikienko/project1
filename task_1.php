fd4c26e81e4814bce55c9705ae6a31f1e4ce0f37
Max <maksanikienko30@gmail.com>
2022-08-05 19:33:32 +0300

    Task_2 done

--- task_1.php
+++ task_1.php
@@ -1,27 +1,16 @@
 <?php
-//[ "task_1.php", "users.csv", "Andrew" ] <= php task_1.php user.csv Andrew
-print_r($argv);
-$script = $argv[0];
-$filename = $argv[1];
-$username = $argv[2];
 
+$handle = fopen("users.csv", "r+");
+
+$rowOne = fgetcsv($handle, 0, ",");
+$rowTwo = fgetcsv($handle, 0, ",");
+$value= $argv[1];
+
+while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+$result  = array_combine($rowOne,$data); 
  
-print "Searching $username in $filename".PHP_EOL;
-
-// (filename, name) => user[]
-function filter_by_name(string $filename, string $username): array
-{
-    $result = []; // [stringa, stringb, 1,2,3,]
-    if (($handle = fopen($filename, "r")) !== FALSE) {
-        while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-            if (str_contains($data[0], $username)) {
-                $result[] = $data;
-                // array_push($result, $data);
-            }
-        }
-        fclose($handle);
+    if (str_contains($result['firstName'],$value))
+       print_r($result);
     }
 
-    return $result;
-}
-print_r($result);
+?>
--- task_2.php
+++ task_2.php
@@ -1,15 +1,20 @@
 <?php
-
+if(count($argv) < 3) {
+  print "Usage : php task_2.php <fieldName> <value>".PHP_EOL;
+  die();  }
 $handle = fopen("users.csv", "r+");
 
 $rowOne = fgetcsv($handle, 0, ",");
 $rowTwo = fgetcsv($handle, 0, ",");
-$value= 'Gladys';
+$value= $argv[2];
+$fieldName = $argv[1];
+
+
 
 while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
 $result  = array_combine($rowOne,$data); 
  
-    if (str_contains($result['firstName'],$value))
+    if (str_contains($result[$argv[1]],$value))
        print_r($result);
     }
 
