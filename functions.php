cbb091cb6048e1b365fdc6c8f044ec8974f0dc34
Alexandr Harabara <dev.xandr@gmail.com>
2022-08-12 09:56:21 +0300

    Formatting and help for the script

--- functions.php
+++ functions.php
@@ -1,93 +1,87 @@
 <?php
-        //add
-function add_item(string $content){
-            // сюда содержимое файла
-if (!file_exists("./php-calls.json")) { 
-    $arr = array();
-    $string = json_encode($arr);
-    file_put_contents("./php-calls.json",$string);
-}
 
-$file = file_get_contents("./php-calls.json");
+function add_item(string $content)
+{
+    // сюда содержимое файла
+    if (!file_exists("./php-calls.json")) {
+        $arr = array();
+        $string = json_encode($arr);
+        file_put_contents("./php-calls.json", $string);
+    }
 
-$data = json_decode($file);
-
-$data[] = ['id'=> uniqid(""), 'content' => $content,'timestump'=> date("20-12-2000")];
-
-$data = json_encode($data, JSON_PRETTY_PRINT);
-
-file_put_contents("./php-calls.json",$data);  
-        };
-
-        //update
-function update_item(string $id,string $content){
-        
-        if (!file_exists("./php-calls.json")) { 
-                $arr = array();
-                $string = json_encode($arr);
-                file_put_contents("./php-calls.json",$string);
-         }
-        
-        $file = file_get_contents("./php-calls.json");
-        $data = json_decode($file, true);
-        
-        $current = $id;
-        foreach ( $data as $key => $value){   
-        
-           if ($value['id'] === $current) {          
-            $data[$key]['content'] = $content;
-           }
-        } 
-        
-        $data = json_encode($data, JSON_PRETTY_PRINT);
-        
-        file_put_contents("./php-calls.json",$data);
-    };
-
-                //delete
-                function delete_item($content){
-    
-    if (!file_exists("./php-calls.json")) { 
-            $arr = array();
-            $string = json_encode($arr);
-            file_put_contents("./php-calls.json",$string);
-     }
-    
     $file = file_get_contents("./php-calls.json");
-    
+    $data = json_decode($file);
+    $data[] = ['id' => uniqid(""), 'content' => $content, 'timestump' => date("20-12-2000")];
+    $data = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("./php-calls.json", $data);
+};
+
+function update_item(string $id, string $content)
+{
+    if (!file_exists("./php-calls.json")) {
+        $arr = array();
+        $string = json_encode($arr);
+        file_put_contents("./php-calls.json", $string);
+    }
+
+    $file = file_get_contents("./php-calls.json");
+    $data = json_decode($file, true);
+
+    $current = $id;
+    foreach ($data as $key => $value) {
+
+        if ($value['id'] === $current) {
+            $data[$key]['content'] = $content;
+        }
+    }
+
+    $data = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("./php-calls.json", $data);
+};
+
+function delete_item($content)
+{
+    if (!file_exists("./php-calls.json")) {
+        $arr = array();
+        $string = json_encode($arr);
+        file_put_contents("./php-calls.json", $string);
+    }
+
+    $file = file_get_contents("./php-calls.json");
     $data = json_decode($file, true);
     $current = $content;
-    foreach ( $data as $key => $value){      
-    
-       if ($value['id'] === $current) {          
-    
-        unset($data[$key]);        
-       }
-     } 
-        
+    foreach ($data as $key => $value) {
+        if ($value['id'] === $current) {
+            unset($data[$key]);
+        }
+    }
+
     $data = json_encode($data, JSON_PRETTY_PRINT);
-    
-    file_put_contents("./php-calls.json",$data);
-    };
+    file_put_contents("./php-calls.json", $data);
+};
 
+function read_item()
+{
+    if (!file_exists("./php-calls.json")) {
+        $arr = array();
+        $string = json_encode($arr);
+        file_put_contents("./php-calls.json", $string);
+    }
 
-            //read
-            function read_item(){
-            if (!file_exists("./php-calls.json")) { 
-                $arr = array();
-                $string = json_encode($arr);
-                file_put_contents("./php-calls.json",$string);
-         }
-        
-        $file = file_get_contents("./php-calls.json");
-        
-        $data = json_decode($file);
-        
-        $data = json_encode($data, JSON_PRETTY_PRINT);
-        
-        file_put_contents("./php-calls.json",$data);
-        print_r($data);
-            };
-        
+    $file = file_get_contents("./php-calls.json");
+    $data = json_decode($file);
+    $data = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents("./php-calls.json", $data);
+    print_r($data);
+};
 
-?>
+function get_help() {
+    return 
+    "Usage:    php script.php <command> <...arguments>" . PHP_EOL .
+    "Commands:" . PHP_EOL .
+    "   read                  - display records from the storage"  . PHP_EOL .
+    "   add    <content>      - add a records to the storage" . PHP_EOL .
+    "   edit   <id> <content> - edit a records from the storage" . PHP_EOL .
+    "   delete <id>           - delete a records from the storage" . PHP_EOL .
+    "   help                  - display this message" . PHP_EOL;
+}
